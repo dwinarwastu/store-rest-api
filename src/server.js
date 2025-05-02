d45025775e3abbd9b04938b7dbc7c7a6f0a33515
dwinarwastu <dwinarwastu02@gmail.com>
2025-05-02 09:21:17 +0800

    feat: morgan

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/db.js";
+import morgan from "morgan";
 import ProductRouter from "./product/product.route.js";
 import CategoryRouter from "./category/category.route.js";
 import AuthRouter from "./user/user.route.js";
@@ -15,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/v1/products", ProductRouter);
 app.use("/api/v1/categories", CategoryRouter);
