dbcb5948d05ae6e355a2c33e5471f72ab0b4e557
dwinarwastu <dwinarwastu02@gmail.com>
2025-05-02 10:34:43 +0800

    feat: helmet

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/db.js";
 import morgan from "morgan";
+import helmet from "helmet";
 import ProductRouter from "./product/product.route.js";
 import CategoryRouter from "./category/category.route.js";
 import AuthRouter from "./user/user.route.js";
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(helmet());
 
 app.use("/api/v1/products", ProductRouter);
 app.use("/api/v1/categories", CategoryRouter);
